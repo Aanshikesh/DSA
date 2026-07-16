9866d495c9cf6954929be131dc44cd9ce59538a3
AANSHIKESH RAWAT <sbrawat790@gmail.com>
2026-07-16 11:46:10 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.14%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+    // int e = n*(n+1);
+    // int o = n*n;
+    //  while (e != 0) {
+    //         int temp = e;
+    //         e= o % e;
+    //         o= temp;
+    //     }
+
+    return n;  
+    }
+}
