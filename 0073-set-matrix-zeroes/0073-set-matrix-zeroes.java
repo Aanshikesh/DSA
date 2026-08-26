8a1914284bfbd2da9792bf801e9d5d4cf527136f
AANSHIKESH RAWAT <sbrawat790@gmail.com>
2026-08-26 11:20:18 +0530

    Time: 1 ms (97.4%), Space: 46.9 MB (99.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,25 @@
-import java.util.ArrayList;
-
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<int[]> list = new ArrayList<>();
-        int n = matrix.length;
-        int m = matrix[0].length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    list.add(new int[]{i, j});  
+        boolean zeroinFirstCol = false;
+        for (int row = 0; row < matrix.length; row++) {
+            if (matrix[row][0] == 0) zeroinFirstCol = true;
+            for (int col = 1; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
                 }
             }
         }
-        int x = list.size();
-        for(int i =0;i<x;i++){
-            changezero(matrix,list.get(i)[0],list.get(i)[1] , n,m);
-        }
 
-    }
-    private void changezero(int[][] matrix, int i ,int j , int n , int m){
-        for(int a=0;a<m;a++){
-            matrix[i][a]=0;
-        }
-        for(int a=0;a<n;a++){
-            matrix[a][j]=0;
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int col = matrix[0].length - 1; col >= 1; col--) {
+                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
+                    matrix[row][col] = 0;
+                }
+            }
+            if (zeroinFirstCol) {
+                matrix[row][0] = 0;
+            }
         }
     }
 }
