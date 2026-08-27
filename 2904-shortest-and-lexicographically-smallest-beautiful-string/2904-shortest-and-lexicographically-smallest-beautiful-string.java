a99e0012644f00a0b0c772dd56d3d83c51b30111
AANSHIKESH RAWAT <sbrawat790@gmail.com>
2026-08-27 22:05:01 +0530

    Time: 9 ms (8.78%), Space: 45.8 MB (8.78%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,36 +1,34 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        String ans = "";
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
-
-            int oneCnt = 0;
-            StringBuilder cur = new StringBuilder();
-
-            for (int j = i; j < n; j++) {
-
-                cur.append(s.charAt(j));
-
-                if (s.charAt(j) == '1')
-                    oneCnt++;
-
-                if (oneCnt > k)
+        int n=s.length();
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int count=0;
+            StringBuilder sb=new StringBuilder();
+            for(int j=i;j<n;j++){
+                if(s.charAt(j)=='1'){
+                    count++;
+                    sb.append(1);
+                }
+                if(s.charAt(j)=='0' && sb.length()>0){
+                    sb.append(s.charAt(j));
+                }
+                if(k==count){
+                    list.add(sb.toString());
                     break;
-
-                if (oneCnt == k) {
-                    String curStr = cur.toString();
-
-                    if (ans.isEmpty() ||
-                        curStr.length() < ans.length() ||
-                        (curStr.length() == ans.length() && curStr.compareTo(ans) < 0)) {
-
-                        ans = curStr;
-                    }
                 }
             }
         }
-
+        if(list.size()==0) return "";
+        Collections.sort(list);
+        String ans=list.get(0);
+        for(String str:list){
+            if(str.length()<ans.length() ||
+                (str.length()==ans.length() &&
+                 str.compareTo(ans)<0)){
+                ans=str;
+            }
+        }
         return ans;
     }
 }
