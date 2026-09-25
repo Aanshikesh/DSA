97bbf85de34a1f07d4424d636ee3a308d84de3f2
AANSHIKESH RAWAT <sbrawat790@gmail.com>
2026-09-25 22:49:25 +0530

    Time: 1 ms (40.14%), Space: 53.9 MB (46.33%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    int ans =0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null) return 0;
+        if(root.val>=low && root.val<=high) ans+=root.val;
+        rangeSumBST(root.left, low, high);
+        rangeSumBST(root.right,  low, high);
+        return ans;
+    }
+}
