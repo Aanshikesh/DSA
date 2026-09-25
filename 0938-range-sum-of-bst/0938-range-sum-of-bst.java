d4e756f827af8c7c9dbcd59caa5b4a4aaeed7952
AANSHIKESH RAWAT <sbrawat790@gmail.com>
2026-09-25 22:51:56 +0530

    Time: 0 ms (100%), Space: 53.9 MB (46.33%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -4,8 +4,8 @@ class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root==null) return 0;
         if(root.val>=low && root.val<=high) ans+=root.val;
-        rangeSumBST(root.left, low, high);
-        rangeSumBST(root.right,  low, high);
+        if(root.val>=low)rangeSumBST(root.left, low, high);
+        if(root.val<=high)rangeSumBST(root.right,  low, high);
         return ans;
     }
 }
